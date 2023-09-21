e169170720b904a7af6dec79ab19dceed1f5b4b5
i0ji <ioji@inbox.ru>
2023-09-21 17:56:53 +0300

    21.09 styling attempts

--- src/components/DropDownMenu/DropDownMenu.tsx
+++ src/components/DropDownMenu/DropDownMenu.tsx
@@ -1,6 +1,7 @@
-import React from "react";
-import "./DropDownMenu.scss";
 import Select from "react-select";
+import styles from "./DropDownMenu.module.scss";
+//import "./DropDownMenu.scss"
+// import { DropDownMenuStyles } from "./DropDownMenuStyles";
 
 const options = [
   { value: "sl", label: "s1mple l33t" },
@@ -15,10 +16,20 @@ export default function DropDownMenu(props: any) {
 
   return (
     <Select
+      classNames={{
+        control: (state) => {
+          state.isFocused ? "border-red-600" : "border-grey-300"},
+        option: () => {
+          backgroundColor: 'red'
+        }  
+      }}
       value={props.selectedLevel}
       defaultValue={options[0]}
       onChange={handleLevelChange}
       options={options}
+      // classNamePrefix={'dd'}
+      menuPlacement={"top"}
+      hideSelectedOptions={true}
     />
   );
 }
---
+++ src/components/DropDownMenu/DropDownMenuStyles.ts
@@ -0,0 +1,46 @@
+
+// export const DropDownMenuStyles = {
+//     option: (provided: any, state: any) => ({
+//         fontSize: '70%',
+//         backgroundColor: 'rgba(0, 255, 255, 0.5)',
+//         border: 'none',
+//         color: state.isSelected ? 'rgba(0, 255, 255, 0.5)' : 'black',
+//         padding: 20,
+//     }),
+//     container: (provided: any, state: any) => ({
+//         minWidth: '80%',
+//         width: '90%',
+//         backgroundColor: '#333',
+//         border: 'none',
+//         borderRadius: '15px',
+//         display: 'flex',
+//         justifyContent: 'center',
+//         alignItems: 'center'
+//     }),
+//     control: (provided: any, state: any) => ({
+//         minWidth: '80%',
+//         backgroundColor: '#333',
+//         color: 'rgba(0, 255, 255, 0.5)',
+//     }),
+//     valueContainer: (provided: any) => ({
+//         background: '#333',
+//         borderRadius: '10px',
+//         height: '10%',
+//         minWidth: '80%',
+//         boxShadow: '10px 10px 10px rgba(0, 255, 255, 0.5)'
+//     }),
+//     menu: () => ({
+//         backgroundColor: '#333',
+//         color: 'rgba(0, 255, 255, 0.5)',
+//     }),
+//     indicator: (provided: any, state: any) => ({
+//         display: 'none',
+//         minWidth: '80%',
+//         backgroundColor: 'red',
+//         color: 'rgba(0, 255, 255, 0.5)',
+//     }),
+//     select__control: () => ({
+//         backgroundColor: 'red'
+//     })
+// };
+export {}
