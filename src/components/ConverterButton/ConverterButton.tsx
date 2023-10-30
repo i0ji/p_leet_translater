78bfbbdddbb7d56d6845ac1cdee4c946b96d6abd
i0ji <ioji@inbox.ru>
2023-10-30 11:21:27 +0300

    ч

--- src/components/ConverterButton/ConverterButton.tsx
+++ src/components/ConverterButton/ConverterButton.tsx
@@ -4,7 +4,7 @@ import styles from "./ConvertButton.module.scss"
 export default function ConvertButton(props: any) {
     return (
         <button className={styles.ConvertButton} onClick={props.onClick}>
-            ROLL ON!
+            ROLL ON!!!
         </button>
     )
 }
