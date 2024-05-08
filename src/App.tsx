c10c6aebb0aa0f08f2a4ee0673e0ff5de8464f63
i0ji <ioji@inbox.ru>
2024-05-08 15:32:41 +0300

    feature/global: yarn->4*; multiply updates; GH-pages set;

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
-
-import styles from './App.module.scss';
+import styles from "./App.module.scss";
 import TextBlock from './components/TextBlock/TextBlock';
 
 export default function App() {
-  return (
-    <div className={styles.App}>
-      <TextBlock/>
-    </div>
-  );
+    return (
+        <div className={styles.App}>
+            <TextBlock/>
+        </div>
+    );
 }
