fdc4ffc2779ca44412f718a7b8b3f3b0ba0d88e4
i0ji <ioji@inbox.ru>
2025-12-10 15:49:15 +0300

    gh-pages reset

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-   base: '/p_leet_translater/'
+  base: '/p_leet_translater/',
+  build: {
+    outDir: 'build',
+  },
 });
